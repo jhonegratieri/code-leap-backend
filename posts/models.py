88286e5d391ebab46a5badd7de97952518925e50
jhonegratieri <jhonatansimoni@gmail.com>
2024-03-11 11:53:18 -0300

    Create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=30)
+    content = models.CharField(verbose_name="Content", max_length=50)
+    username = models.CharField(verbose_name="Username", max_length=15)
+    create_datetime = models.DateTimeField(auto_now_add=True)
