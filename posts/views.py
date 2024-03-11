03adfe0fb4f571f0ce5d93019df422e87917d1ce
jhonegratieri <jhonatansimoni@gmail.com>
2024-03-11 14:23:09 -0300

    Create CRUD ViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from posts.models import Post
+from posts.serializers import PostSerializer, PostUpdateSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    http_method_names = ["get", "options", "head", "patch", "post", "delete"]
+
+    def get_serializer_class(self):
+        return (
+            PostUpdateSerializer if self.request.method == "PATCH" else PostSerializer
+        )
+
+    def get_object(self):
+        pk = self.kwargs.get("pk", "")
+        obj = get_object_or_404(self.get_queryset(), pk=pk)
+
+        return obj
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+    def partial_update(self, request, *args, **kwargs):
+        post = self.get_object()
+        serializer = self.get_serializer(
+            instance=post,
+            data=request.data,
+            many=False,
+            partial=True,
+            context={"request": request},
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
