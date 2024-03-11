fda3d1db972761865eb8c1359e3723cee5709f70
jhonegratieri <jhonatansimoni@gmail.com>
2024-03-11 14:25:08 -0300

    Create posts urls router

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
-app_name = "core"
+app_name = "posts"
+
+post_api_router = SimpleRouter()
+post_api_router.register(
+    "careers",
+    views.PostViewSet,
+    basename="post-api",
+)
+
+urlpatterns = [
+    path("", include(post_api_router.urls)),
+]
